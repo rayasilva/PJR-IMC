37e88006c93bf8da854407f01f20f1e06125ee4e
rayasilva <rayane2.pereira1b1@gmail.com>
2024-04-11 10:52:16 -0300

    Formulário do 3B1 Lado B

---
+++ SRC/Components/Form/index.js
@@ -0,0 +1,23 @@
+import react from "react";
+import { View,Text,TextInput } from "react-native";
+
+export  default function Form(){
+    return(
+        <View>
+            
+            <View>
+          <Text>Altura</Text>
+           <TextInput
+           placeholder="Ex.1.75" 
+           keyboardType="numeric"/>
+
+
+           <Text>Peso</Text>
+           <TextInput
+           placeholder="Ex.55.766" 
+           keyboardType="numeric"/> 
+           
+             </View>
+        </View>
+    );
+}
---
+++ SRC/Components/Main/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View,Text } from "react-native";
+import Form from "../Form";
+
+export  default function Main(){
+    return(
+        <View>
+         <Form/>
+        </View>
+    );
+}
