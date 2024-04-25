f970bc598cb04d59746b42d4c488d54edaf00d92
rayasilva <rayane2.pereira1b1@gmail.com>
2024-04-25 10:39:57 -0300

    commit do Button e useState

--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,7 +1,15 @@
-import react from "react";
-import { View,Text,TextInput } from "react-native";
+import React, {useState} from "react";
+import { View,Text,TextInput,Button } from "react-native";
 
 export  default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const[weight,setWeight] = useState(null)
+    const[messageImc,setMessageImc] = useState("preenchan seu peso e sua altura");
+    const[imc,setImc] = useState(null)
+    const[textButton,setTextButton] = useState("Calcular IMC")
+
+
     return(
         <View>
             
@@ -16,6 +24,11 @@ export  default function Form(){
            <TextInput
            placeholder="Ex.55.766" 
            keyboardType="numeric"/> 
+
+           <Button
+           title ={textButton}
+           
+           />
            
              </View>
         </View>
